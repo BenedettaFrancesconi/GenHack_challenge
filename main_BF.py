171921f6a19c4f6976cf938967abfb4eae701cee
Benedetta-sys <benedetta.francesconi.1993@gmail.com>
2022-11-27 18:55:24 +0100

    Update main_BF.py

--- main_BF.py
+++ main_BF.py
@@ -2,8 +2,15 @@
 # YOU MUST NOT MODIFY THIS FILE
 ##############################################
 from model import generative_model
+from utils import AD_distance
+import pandas as pd
+import torch
 import numpy as np
 import logging
+
+df = pd.read_csv('../data/df_train.csv')
+train = torch.tensor(df.iloc[:int(-365*n_val_years), 1:].to_numpy())
+val = torch.tensor(df.iloc[int(-365*n_val_years):, 1:].to_numpy())
 
 
 logging.basicConfig(filename="check.log", level=logging.DEBUG, 
@@ -28,7 +35,8 @@ def simulate(noise):
         assert output.shape == (noise.shape[0], 6), "Shape error, it must be (n_data, 6). Please verify the shape of the output."
         
         # write the output
-        np.save("output.npy", output)
+        #np.save("output.npy", output)
+        dist = AD_distance(val, output)
 
     except Exception as e:
         message = e
@@ -36,7 +44,8 @@ def simulate(noise):
     finally:
         logging.debug(message)
 
-    return output
+    return dist
+
 
     
 if __name__ == "__main__":
