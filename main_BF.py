52d7c1eef6bbd6fc1976447b1d624eec63cfc11b
Benedetta-sys <benedetta.francesconi.1993@gmail.com>
2022-11-29 19:34:28 +0100

    Update main_BF.py

--- main_BF.py
+++ main_BF.py
@@ -1,17 +1,17 @@
 ##############################################
 # YOU MUST NOT MODIFY THIS FILE
-##############################################
+#############################################
 from model import generative_model
 from utils import AD_distance
 import pandas as pd
-import torch
+#import torch
 import numpy as np
 import logging
 
 #df = pd.read_csv('C:/Users/bened/Documents/GitHub/TEAM_ROCKET/datadf_train.csv')
 
 
-logging.basicConfig(filename="check.log", level=logging.DEBUG, 
+logging.basicConfig(filename="check_BF.log", level=logging.DEBUG, 
                     format="%(asctime)s:%(levelname)s: %(message)s", 
                     filemode='w')
 
@@ -27,16 +27,16 @@ def simulate(noise, df):
         input of the generative model
     """
     train = np.array(df.iloc[:int(-365*9), 1:])
-    val = np.array(df.iloc[int(-365*9):, 1:])
+    val = np.array(df.iloc[int(-365*9):, 1:]).transpose().astype('float32')
 
     try:
         output = generative_model(noise)
         message = "Successful simulation" 
-        assert output.shape == (noise.shape[0], 6), "Shape error, it must be (n_data, 6). Please verify the shape of the output."
+        assert output.shape == (noise.shape[0], 6).transpose().astype('float32'), "Shape error, it must be (n_data, 6). Please verify the shape of the output."
         
         # write the output
-        #np.save("output.npy", output)
-        dist = AD_distance(val, output)
+        np.save("output.npy", output)
+        AD_distance(val, output)
 
     except Exception as e:
         message = e
@@ -44,14 +44,14 @@ def simulate(noise, df):
     finally:
         logging.debug(message)
 
-    return dist
+    return output
 
 
     
 if __name__ == "__main__":
     z = np.random.normal(0,1, size = (10,50))
     noise = z
-    df = np.load("data/df_train.csv")
+    df = pd.read_csv("data/df_train.csv")
     simulate(noise,df)
     
     
