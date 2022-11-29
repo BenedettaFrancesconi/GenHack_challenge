a458e0d47e16b2eac78f75987cee318fd66ad76c
Benedetta-sys <benedetta.francesconi.1993@gmail.com>
2022-11-29 22:59:26 +0100

    Update main_BF.py

--- main_BF.py
+++ main_BF.py
@@ -1,6 +1,3 @@
-##############################################
-# YOU MUST NOT MODIFY THIS FILE
-#############################################
 from model import generative_model
 from utils import AD_distance
 import pandas as pd
@@ -15,7 +12,7 @@ logging.basicConfig(filename="check_BF.log", level=logging.DEBUG,
                     format="%(asctime)s:%(levelname)s: %(message)s", 
                     filemode='w')
 
-def simulate(noise, df):
+def simulate(df, noise):
     """
     simulation of your Generative Model
 
@@ -27,33 +24,33 @@ def simulate(noise, df):
         input of the generative model
     """
     
-    try:
-        train = np.array(df.iloc[:int(-365*9), 1:])
-        val = np.array(df.iloc[int(-365*9):, 1:]).transpose().astype('float32')
-        noise = torch.tensor(noise).float()
-        output = np.array(generative_model(noise))
-        #message = "Successful simulation" 
-        #assert output.shape == (noise.shape[0], 6).transpose().astype('float32'), "Shape error, it must be (n_data, 6). Please verify the shape of the output."
+
+    #train = np.array(df.iloc[:int(-365*9), 1:])
+    val = np.array(df.iloc[int(-365*9):, 1:]).transpose().astype('float32')
+    #noise = torch.tensor(noise).float()
+    output = np.array(generative_model(noise)).transpose().astype('float32')
+    #message = "Successful simulation" 
+    #assert output.shape == (noise.shape[0], 6).transpose().astype('float32'), "Shape error, it must be (n_data, 6). Please verify the shape of the output."
         
-        # write the output
-        #np.save("output.npy", output)
-        w = []
-        w = AD_distance(val, output)
+    
+    w = []
+    """
+    AD_distance:
+    real: np.array(6, n_test)
+    fake: np.array(6, n_test)
+    """
+    
 
-    except Exception as e:
-        message = e
-                
-    finally:
-        logging.debug(message)
-
+    w = AD_distance(output, val)
+    
     return w
 
 
     
 if __name__ == "__main__":
     z = np.random.normal(0,1, size = (10,50))
-    noise = z
+    noise = torch.tensor(z).float()
     df = pd.read_csv("data/df_train.csv")
-    simulate(noise,df)
+    simulate(df, noise)
     
     
