cbca0a66c4eb5a84ebeae66d452be843884c44be
Benedetta-sys <benedetta.francesconi.1993@gmail.com>
2022-12-01 20:06:18 +0100

    Update train_diffusion.py

--- train_diffusion.py
+++ train_diffusion.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from sklearn.linear_model import LinearRegression
 import torch
 from torch import optim
 from torch.optim.lr_scheduler import StepLR  
@@ -9,6 +10,7 @@ from utils import AD_distance
 from diffusion_train_loops import train_epoch_diffusion, eval_epoch_diffusion
 from diffusion import GaussianDiffusion1D, Unet1D
 # from architectures.mlp import Diffusion_MLP
+
 
 def load_data(data_path='data/df_train.csv', 
               n_val_years=9,
@@ -28,6 +30,23 @@ def load_data(data_path='data/df_train.csv',
 
     return train_loader, val_loader
 
+# BENEDETTA KICKS IN 
+li = {'s1': df['s1'], 's2': df['s2'], 's3': df['s3'], 's4': df['s4'], 's5': df['s5'], 's6': df['s6']}
+trends = pd.DataFrame()
+for name, value in li.items():
+    
+    series = df[name]
+    X = [i for i in range(0, len(series))]
+    X = np.reshape(X, (len(X), 1))
+    y = series.values
+    model = LinearRegression()
+    model.fit(X, y)
+    # calculate trend
+    trends[name] = model.predict(X)
+    # detrend
+    #detrended = [y[i]-trend[i] for i in range(0, len(series))] maybe I'll use it, I don't know
+    
+#
 
 def create_model(config, device):
     # model = Diffusion_MLP(latent_dim=config['latent_dim'], n_cin=6)
@@ -56,20 +75,20 @@ def log(key, val):
 def main():
     config = {
         'wandb_on': False,
-        'lr': 1e-3,
+        'lr': 1e-1,
         'adam_betas': (0.9, 0.99),
         'momentum': 0.9,
         'batch_size': 256,
-        'max_epochs': 1000,
+        'max_epochs': 100,
         'AD_epochs': 100,
         'n_AD_years': 9,
         'eval_epochs': 5,
         'eval_batches': 100,
         'n_val_years': 9,
         'seed': 1,
-        'latent_dim': 32, # must be multiple of 8
+        'latent_dim': 8, # must be multiple of 8
         'input_dim': 6,
-        'timesteps': 1000,
+        'timesteps': 100,
         'loss_type': 'l1',     # L1 or L2
         'checkpoint_path': 'checkpoints/diffusion_1000e_lr1e-3_val9_d10.tar',
         'data_path': 'data/df_train.csv',
@@ -123,6 +142,7 @@ def main():
             sample = sample.squeeze().detach().cpu().numpy().astype('float32') # (n_test, 6)
 
             ## TODO: Add output of ARIMA to sample here
+            # How do I add the values from the dataframe trends in line 35? Its shape is (9618, 6)
             # sample = sample + (ARIMA(2006) - mean(ARIMA(1981 to 2002)))
 
             w = AD_distance(sample.reshape(-1, 6).transpose(), val_data.cpu().numpy().astype('float32').transpose())
